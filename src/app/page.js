78c89472607cf20aaeccd7ac44690e9e18d054c7
Deepak <deepak2603dk@gmail.com>
2024-04-19 02:57:36 +0530

    minor change

--- src/app/page.js
+++ src/app/page.js
@@ -1,5 +1,5 @@
 import Image from "next/image";
-import Mainpg from "@/app/_components/Mainpg";
+import Mainpg from "./_components/Mainpg";
 import Navbar from "./_components/Navbar";
 export default function Home() {
   return (
