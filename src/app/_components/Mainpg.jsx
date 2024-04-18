168b4b4f2af4a73a826ab50745ba0df5ef2a998a
Deepak <deepak2603dk@gmail.com>
2024-04-19 03:25:06 +0530

    main page ui

--- src/app/_components/Mainpg.jsx
+++ src/app/_components/Mainpg.jsx
@@ -2,7 +2,14 @@ const Mainpg = () => {
     return (  
         <>
         <div className="w-full ">
-        <input type="text" placeholder="Enter Url" />
+            <h1 className=" text-4xl md:text-7xl font-medium text-white text-center pt-20">URL<span className="italic font-medium text-violet-500">SHORTNER</span></h1>
+       
+       <div className=" w-full  flex justify-center">
+        <input 
+        className="rounded-md w-[80%] md:w-[50%] h-9 px-3 mt-4 border-violet-400 focus:ring-violet-300 focus:border-violet-300 dark:focus:ring-blue-600 dark:ring-offset-gray-800"
+        type="text" 
+        placeholder="Enter Url" />
+        </div>
         </div>
         </>
     );
--- src/app/page.js
+++ src/app/page.js
@@ -5,7 +5,6 @@ export default function Home() {
   return (
    <>
    <div className="w-full h-screen bg-slate-800">
-   <Navbar/>
    <Mainpg/>
 
    </div>
