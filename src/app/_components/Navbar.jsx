79d3082000fa480e8e007d64f997491e6d489c61
Deepak <deepak2603dk@gmail.com>
2024-04-19 02:46:42 +0530

    removing button

--- src/app/_components/Navbar.jsx
+++ src/app/_components/Navbar.jsx
@@ -1,15 +1,9 @@
 import Link from "next/link";
-import { buttonVariants,button } from "@/components/ui/button";
 const Navbar = () => {
     return ( 
         <>
         <div className="py-2  border-b-[1px] border-slate-200/25 px-5 ">
-        <ul>
-            <li>
-                <Link className="button()" href='auth/signIn'>Login</Link>
-            </li>
-
-        </ul>
+        
         </div>
         </>
      );
