a4d2f5e3a6791d4d63ca83702842520234eb9f57
Deepak <deepak2603dk@gmail.com>
2024-04-26 23:05:49 +0530

    fixing bugs

--- src/app/auth/signin/page.js
+++ src/app/auth/signin/page.js
@@ -5,7 +5,6 @@ import { useRouter } from "next/navigation";
 const SignIn = () => {
   const [errorMsg, setErrorMsg] = useState("error message");
   const [inputValue, setInputValue] = useState({
-    name: "",
     email: "",
     password: "",
   });
@@ -19,24 +18,17 @@ const SignIn = () => {
     });
   };
 
-  const navigateToSignIn = () => {
-    router.push("/auth/signin");
+  const navigateToSignUp = () => {
+    router.push("/auth/signup");
   };
 
   return (
     <>
       <div className="grid place-items-center h-screen w-full bg-violet-50 ">
         <div className="shadow-lg border-t-8 border-violet-500 rounded-md p-5 w-[90%] sm:w-[25rem]">
-          <h1 className="text-center font-semibold text-xl">Register</h1>
+          <h1 className="text-center font-semibold text-xl">Login</h1>
           <form className="flex gap-4 flex-col mt-5">
-            <input
-              type="text"
-              value={inputValue.name}
-              name="name"
-              onChange={onchangeHandler}
-              placeholder="Enter Your Name"
-              className="block flex-1 text-black border-0 bg-violet-100 py-1.5 px-2 rounded-md text-white-900 placeholder:text-gray-400 focus:ring-0 focus:border-0 sm:text-sm sm:leading-6"
-            />
+           
             <input
               type="text"
               value={inputValue.email}
@@ -54,7 +46,7 @@ const SignIn = () => {
               className="block flex-1 text-black border-0 bg-violet-100 py-1.5 px-2 rounded-md text-white-900 placeholder:text-gray-400 focus:ring-0 focus:border-0 sm:text-sm sm:leading-6"
             />
             <button className="bg-violet-500 py-2 rounded-md text-white font-semibold hover:bg-violet-600">
-              Register
+              Login
             </button>
           </form>
           <div className="mt-2">
@@ -62,9 +54,9 @@ const SignIn = () => {
           </div>
           <div className="mt-2">
             <h1 className="text-sm text-center cursor-pointer">
-              Have an account?{" "}
-              <span className="text-violet-500" onClick={navigateToSignIn}>
-                Login
+              Haven't any account?{" "}
+              <span className="text-violet-500" onClick={navigateToSignUp}>
+                Create
               </span>
             </h1>
           </div>
