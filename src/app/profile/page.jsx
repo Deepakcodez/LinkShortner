18d0b9475fbe9e9c6935ed7dcb0e3f3a2142c59d
Deepak <deepak2603dk@gmail.com>
2024-05-30 00:46:52 +0530

    code card on profile page

--- src/app/profile/page.jsx
+++ src/app/profile/page.jsx
@@ -1,22 +1,25 @@
 "use client"
 import React from 'react';
 import { getSession, useSession } from 'next-auth/react';
-// import { auth } from '../../auth';
-import Userlinks from '../_components/UserLinks';
 
 
-const Profile =  () => {
+const Profile = () => {
   const session = useSession()
-  console.log('>>>>>>>>>>>user deatil profile', session?.data?.user?.name)
   return (
     <>
       <div className="h-screen max-h-[auto] w-full bg-slate-800 pt-3">
         <div className=" px-3 mx-4  border-l-4 border-l-red-500">
-          <h1 className=" text-white  font-semibold text-3xl"> Hello {session?.data?.user.name||"..........."}</h1>
+          <h1 className=" text-white  font-semibold text-3xl"> Hello {session?.data?.user.name || "..........."}</h1>
         </div>
-        
-  <Userlinks/>
-      </div>
+    {/* //card */}
+        <div className='flex justify-center items-center w-full mt-[10rem]'>
+
+          <div className='bg-violet-500/25 py-5 px-3 rounded-lg w-[90%] md:w-[70%] ring-violet-500 ring-1 sm:ps-10 sm:pe-5 flex justify-between'>
+            <h1 className='text-white text-xs sm:text-base truncate'>hello</h1>
+
+          </div>
+
+        </div>      </div>
     </>
   );
 }
